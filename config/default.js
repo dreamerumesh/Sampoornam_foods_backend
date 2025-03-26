7935c4eae32cfb261b3eb1be5c0f625fa46d5471
Umesh Chandra Gopalasetti <152469004+dreamerumesh@users.noreply.github.com>
2025-03-26 14:10:00 +0530

    Update default.js

--- config/default.js
+++ config/default.js
@@ -1,21 +1,3 @@
-// module.exports = {
-//     PORT: process.env.PORT || 5000,
-//     MONGO_URI: "mongodb://127.0.0.1:27017/mydatabase" || process.env.MONGO_URI || 'mongodb://localhost:27017/sampoornam-foods',
-//     JWT_SECRET: process.env.JWT_SECRET || 'sampoornam-secret-key',
-//     JWT_EXPIRE: process.env.JWT_EXPIRE || '7d',
-//     EMAIL_HOST: process.env.EMAIL_HOST,
-//     EMAIL_PORT: process.env.EMAIL_PORT,
-//     EMAIL_USER: process.env.EMAIL_USER,
-//     EMAIL_PASS: process.env.EMAIL_PASS,
-//     ADMIN_EMAIL: process.env.ADMIN_EMAIL || 'admin@sampoornamfoods.com',
-//     OTP_EXPIRE: process.env.OTP_EXPIRE || 10, // in minutes
-//     CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:3000',
-//     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-//     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-//     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
-//   };
-
-
 module.exports = {
     PORT: process.env.PORT || 5000,
     MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/myDatabase",
@@ -28,8 +10,8 @@ module.exports = {
     EMAIL_SERVICE: process.env.EMAIL_SERVICE || 'gmail', // Change if using another provider
     EMAIL_HOST: process.env.EMAIL_HOST || 'smtp.gmail.com',
     EMAIL_PORT: process.env.EMAIL_PORT || 587, // 465 for SSL, 587 for TLS
-    EMAIL_USER: process.env.EMAIL_USER || 'umeshchandra.g2005@gmail.com',
-    EMAIL_PASS: process.env.EMAIL_PASS || 'urustenjtktbrszw',
+    EMAIL_USER: process.env.EMAIL_USER || 'user@gmail.com',
+    EMAIL_PASS: process.env.EMAIL_PASS || 'userpassword',
     EMAIL_FROM: process.env.EMAIL_FROM || 'noreply@sampoornamfoods.com',
 
     // Admin Email
